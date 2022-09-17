2bd537058b1c342eace6d171acafca70b36b7d40
nar2p1nk <farrisisqandar@gmail.com>
2022-09-17 16:32:21 +0800

    done parsing data, now parsing data in posts

--- main.py
+++ main.py
@@ -19,17 +19,24 @@ soup = BeautifulSoup(driver.page_source,'html.parser')
 
 divCard = soup.find('section',class_='_3oBlM _2R889')
 
-#postTitle = divCard.contents[0]['aria-label']
+username = divCard.contents[1].contents[3].contents[1].contents[0].contents[1].contents[0].contents[0]['data-username']
 
-#postLink = divCard.contents[0]['href']
+userProfilePic = divCard.contents[1].contents[3].contents[1].contents[0].contents[1].contents[0].contents[0].contents[0]['src']
 
-#postImage = divCard.contents[0].contents[0].contents[0]['src']
+userProfileLink = divCard.contents[1].contents[3].contents[1].contents[0].contents[1].contents[0].contents[0]['href']
 
-#postImageSrcSet = divCard.contents[0].contents[0].contents[0]['srcset']
+postTitle = divCard.contents[0]['aria-label']
 
-print(divCard)
+postLink = divCard.contents[0]['href']
 
-print(string.whitespace,divCard.contents[0])
+postImage = divCard.contents[0].contents[0].contents[0]['src']
 
-print(string.whitespace,divCard.contents[1])
+postImageSrcSet = divCard.contents[0].contents[0].contents[0]['srcset']
+
+driver.get(postLink)
+
+soup2 = BeautifulSoup(driver.page_source,'html.parser')
+
+print(soup2)
+
 
