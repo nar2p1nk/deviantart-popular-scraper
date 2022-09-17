fb3b3d2e3dcb9765a9dec038244dd89e79dfdedf
nar2p1nk <farrisisqandar@gmail.com>
2022-09-17 14:35:19 +0800

    found a better way of parsing data, implementing solution now

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import sqlite3
-
+import string
 url = 'https://www.deviantart.com/popular/deviations'
 
 htmlClass = 'uU5En = deviantion title| MvjoN = user data| ._3_LJY = image|_121Hz'
@@ -18,53 +17,19 @@ driver.get(url)
 
 soup = BeautifulSoup(driver.page_source,'html.parser')
 
-responseImages = soup.find_all('div',class_='_3_LJY')
+divCard = soup.find('section',class_='_3oBlM _2R889')
 
-responseTitle = soup.find_all('h2',class_='_3CpJS')
+#postTitle = divCard.contents[0]['aria-label']
 
-responseUsername = soup.find_all('span',class_='_2EfV7')
+#postLink = divCard.contents[0]['href']
 
-responseUserProfile = soup.find_all('img',class_='_121Hz')
+#postImage = divCard.contents[0].contents[0].contents[0]['src']
 
-responseUserLink = soup.find_all('a',class_='user-link')
+#postImageSrcSet = divCard.contents[0].contents[0].contents[0]['srcset']
 
-#responseTitleAndUserData = soup.find_all('div',class_='_34lVt')
+print(divCard)
 
-data = []
+print(string.whitespace,divCard.contents[0])
 
-y = 0
-
-
-con = sqlite3.connect('deviantart.db')
-
-cur = con.cursor()
-
-cur.execute('''create TABLE IF NOT EXISTS posts(
-title TEXT NOT NULL,
-images TEXT NOT NULL,
-username TEXT NOT NULL,
-userProfilePicture TEXT NOT NULL,
-userProfileLink TEXT NOT NULL
-            )''')
-
-for x in responseTitle:
-
-    parse = {
-        'title':x.get_text(),
-        'images':responseImages[y].img['src'],
-        'username':responseUsername[y].get_text(),
-        'user profile':responseUserProfile[y]['src'],
-        'user profile link':'https://www.deviantart.com/' + responseUsername[y].get_text()
-    }
-
-    cur.execute('''INSERT INTO posts) VALUES(%s,%s,%s,%s,https://www.deviantart.com/%s)'''% (x.get_text(),responseImages[y].img['src'],responseUsername[y].get_text(),responseUserProfile[y]['src'],responseUsername[y].get_text()))
-
-    print(parse)
-
-    data.append(parse)
-
-    y += 1
-
-
-
+print(string.whitespace,divCard.contents[1])
 
