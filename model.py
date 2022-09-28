e3a4232eebd680f18beac45012acffad317a2f0d
nar2p1nk <farrisisqandar@gmail.com>
2022-09-29 00:48:37 +0800

    --no-edit

--- model.py
+++ model.py
@@ -15,11 +15,33 @@ class User(BaseModel):
 
 class Post(BaseModel):
     postId = AutoField(primary_key=True)
-    displayImageLink = CharField()
-    originalImageLink = CharField()
+#    displayImageLink = CharField()
+#    originalImageLink = CharField()
     numFavourites = IntegerField(default=0)
     numViews = IntegerField(default=0)
     numComments =  IntegerField(default=0)
     PostedBy = ForeignKeyField(User, backref='posts')
     timePosted = DateTimeField()
 
+
+class OtherUser(BaseModel):
+    otherId = AutoField(primary_key=True)
+    username = CharField(unique=True)
+    profilePictureLink = CharField()
+    userProfileLink = CharField()
+    postLink = CharField()
+
+class OtherPost(BaseModel):
+    postId = AutoField(primary_key=True)
+    displayImageLink = CharField()
+    originalImageLink = CharField()
+    numFavourites = IntegerField(default=0)
+    numViews = IntegerField(default=0)
+    numComments =  IntegerField(default=0)
+    PostedBy = ForeignKeyField(OtherUser, backref='posts')
+    timePosted = DateTimeField()
+
+
+db.connect()
+
+db.create_tables([User,Post,OtherUser,OtherPost])
