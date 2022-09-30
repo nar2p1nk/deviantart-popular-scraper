1a2f2505331582849d2884803a055d797fea535d
nar2p1nk <farrisisqandar@gmail.com>
2022-10-01 04:40:28 +0800

    YES, can now finally download pictures, now i get to play god

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import string
+import requests
+import shutil
 
 ws = string.whitespace
 
@@ -19,44 +21,81 @@ driver.get(url)
 
 soup = BeautifulSoup(driver.page_source,'html.parser')
 
-divCard = soup.find('section',class_='_3oBlM _2R889')
+divCards = soup.findAll('section',class_='_3oBlM _2R889')
 
-username = divCard.contents[1].contents[3].contents[1].contents[0].contents[1].contents[0].contents[0]['data-username']
+num = 0
 
-userProfilePic = divCard.contents[1].contents[3].contents[1].contents[0].contents[1].contents[0].contents[0].contents[0]['src']
+for divCard in divCards:
 
-userProfileLink = divCard.contents[1].contents[3].contents[1].contents[0].contents[1].contents[0].contents[0]['href']
+    username = divCard.contents[1].contents[3].contents[1].contents[0].contents[1].contents[0].contents[0]['data-username']
+    
+    userProfilePic = divCard.contents[1].contents[3].contents[1].contents[0].contents[1].contents[0].contents[0].contents[0]['src']
+    
+    userProfileLink = divCard.contents[1].contents[3].contents[1].contents[0].contents[1].contents[0].contents[0]['href']
+    
+    postTitle = divCard.contents[0]['aria-label']
+    
+    postLink = divCard.contents[0]['href']
+    
+    displayImage = divCard.contents[0].contents[0].contents[0]['src']
+    
+#    postImageSrcSet = divCard.contents[0].contents[0].contents[0]['srcset']
+    
+    driver.get(postLink)
+    
+    soup2 = BeautifulSoup(driver.page_source,'html.parser')
+    
+    originalImage = soup2.find('img',class_='TZM0T _2NIJr')['src']
+    #_1O2S9 _19exo
+    
+    postNumbers = soup2.find('div',class_='_1p-42 _6G8rT _39R1e')
+    
+    numFavourites = postNumbers.contents[0].contents[0].contents[0].contents[0].contents[0].contents[1]
+    
+    numComments = postNumbers.contents[0].contents[1].contents[0].contents[0].contents[0].contents[1].contents[0].contents[0]
+    
+    numViews = postNumbers.contents[0].contents[2].contents[0].contents[1].contents[0]
+    
+    timePosted = soup2.find('span',class_='_12Yqs _1lf7Q').contents[1]
+    
+    datetimePosted = timePosted['datetime'][0:10] + ' ' + timePosted['datetime'][11:19]
+    
+    timePostedTitleUTC = timePosted['aria-label']
+    
+    timePostedTitleGmt =  timePosted['title']
 
-postTitle = divCard.contents[0]['aria-label']
 
-postLink = divCard.contents[0]['href']
+    r = requests.get(displayImage,stream=True)
 
-displayImage = divCard.contents[0].contents[0].contents[0]['src']
+    re = requests.get(originalImage,stream=True)
 
-postImageSrcSet = divCard.contents[0].contents[0].contents[0]['srcset']
+    if r.status_code and re.status_code == 200:
 
-driver.get(postLink)
+        r.raw.decode_content = True
 
-soup2 = BeautifulSoup(driver.page_source,'html.parser')
+        re.raw.decode_content = True
 
-originalImage = soup2.find('img',class_='TZM0T _2NIJr')['src']
+        displayFilename = 'display ' + postTitle
+
+        originalFilename = 'original ' + postTitle
+
+        with open('./images/display/'+postTitle.replace(' ','_').lower(),'wb') as f:
+            shutil.copyfileobj(r.raw,f)
+
+            print('display file: '+postTitle+'. downloaded')
+
+        with open('./images/original/'+postTitle.replace(' ','_').lower(),'wb') as f:
+            shutil.copyfileobj(re.raw,f)
+
+            print('original file: '+postTitle+'. downloaded')
+
+    else:
+        print('err')
+
+    num += 1
+    print(num)
 
 
-postNumbers = soup2.find('div',class_='_1p-42 _6G8rT _39R1e')
-
-numFavourites = postNumbers.contents[0].contents[0].contents[0].contents[0].contents[0].contents[1]
-
-numComments = postNumbers.contents[0].contents[1].contents[0].contents[0].contents[0].contents[1].contents[0].contents[0]
-
-numViews = postNumbers.contents[0].contents[2].contents[0].contents[1].contents[0]
-
-timePosted = soup2.find('span',class_='_12Yqs _1lf7Q').contents[1]
-
-datetimePosted = timePosted['datetime'][0:10] + ' ' + timePosted['datetime'][11:19]
-
-timePostedTitleUTC = timePosted['aria-label']
-
-timePostedTitleGmt =  timePosted['title']
 
 
 
